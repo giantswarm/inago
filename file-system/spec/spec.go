05eabec160086d81c0e7a8c646080399c7aa2628
Tim Schindler <tim@giantswarm.io>
2016-02-17 17:00:55 +0000

    fixed docs

--- file-system/spec/spec.go
+++ file-system/spec/spec.go
@@ -8,12 +8,12 @@ import (
 
 // FileSystem implements file system operations.
 type FileSystem interface {
-	// ReadDir implements os.ReadDir.
+	// ReadDir is the equivalent to os.ReadDir.
 	ReadDir(dirname string) ([]os.FileInfo, error)
 
-	// ReadFile implements ioutil.ReadFile.
+	// ReadFile is the equivalent to ioutil.ReadFile.
 	ReadFile(filename string) ([]byte, error)
 
-	// WriteFile implements ioutil.WriteFile.
+	// WriteFile is the equivalent to ioutil.WriteFile.
 	WriteFile(filename string, bytes []byte, perm os.FileMode) error
 }
