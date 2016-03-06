8b73697c5560e3f15eaa459a29892fae162b0d02
Joseph Salisbury <salisbury.joseph@gmail.com>
2016-03-06 15:44:32 +0000

    Add error cause assertions to validation

--- validator/error.go
+++ validator/error.go
@@ -15,3 +15,43 @@ var (
 	groupsArePrefixError = errgo.New("group is prefix of another group")
 	groupsSameNameError  = errgo.New("group named with same name as another group")
 )
+
+// IsNoUnitsInGroup returns true if the given error cause is noUnitsInGroupError.
+func IsNoUnitsInGroup(err error) bool {
+	return errgo.Cause(err) == noUnitsInGroupError
+}
+
+// IsBadUnitPrefix returns true if the given error cause is badUnitPrefixError.
+func IsBadUnitPrefix(err error) bool {
+	return errgo.Cause(err) == badUnitPrefixError
+}
+
+// IsMixedSliceInstance returns true if the given error cause is mixedSliceInstanceError.
+func IsMixedSliceInstance(err error) bool {
+	return errgo.Cause(err) == mixedSliceInstanceError
+}
+
+// IsMultipleAtInGroupName returns true if the given error cause is multipleAtInGroupNameError.
+func IsMultipleAtInGroupName(err error) bool {
+	return errgo.Cause(err) == multipleAtInGroupNameError
+}
+
+// IsMultipleAtInUnitName returns true if the given error cause is multipleAtInUnitNameError.
+func IsMultipleAtInUnitName(err error) bool {
+	return errgo.Cause(err) == multipleAtInUnitNameError
+}
+
+// IsUnitsSameName returns true if the given error cause is unitsSameNameError.
+func IsUnitsSameName(err error) bool {
+	return errgo.Cause(err) == unitsSameNameError
+}
+
+// IsGroupsArePrefix returns true if the given error cause is groupsArePrefixError.
+func IsGroupsArePrefix(err error) bool {
+	return errgo.Cause(err) == groupsArePrefixError
+}
+
+// IsGroupsSameName returns true if the given error cause is groupsSameNameError.
+func IsGroupsSameName(err error) bool {
+	return errgo.Cause(err) == groupsSameNameError
+}
