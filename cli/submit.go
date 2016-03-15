54c78d8c2d46ef02b005893b783bb9cd068447da
Joseph Salisbury <salisbury.joseph@gmail.com>
2016-03-15 11:26:49 +0000

    Make usage and flag help text a little more consistent

--- cli/submit.go
+++ cli/submit.go
@@ -13,8 +13,8 @@ import (
 var (
 	submitCmd = &cobra.Command{
 		Use:   "submit <group> [scale]",
-		Short: "submit a group",
-		Long:  "submit a group",
+		Short: "Submit a group",
+		Long:  "Submit a group to the cluster, with an optional scale",
 		Run:   submitRun,
 	}
 )
