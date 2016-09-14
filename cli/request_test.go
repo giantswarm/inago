664d6f545a0ca0efbc93cfe93202d1f345bd1b4b
Paweł Kopiczko <kopiczko@gmail.com>
2016-09-14 19:47:12 +0200

    refactor extending request with content tests
    - file mode is 0644 everywhere -- no need to set it everywhere
    - names are shorter, to keep line length sane
    - Unit's Content is unchecked -- to be fixed

--- cli/request_test.go
+++ cli/request_test.go
@@ -11,95 +11,69 @@ import (
 	"github.com/giantswarm/inago/controller"
 )
 
-func givenSomeUnitFileContent() string {
-	return "[Unit]\n" +
-		"Description=Some Unit File Content\n" +
-		"\n" +
-		"[Service]\n" +
-		"ExecStart=/bin/bash -c 'while true; do echo nothing to see, go along; done'\n"
-
-}
-
-type testFileSystemSetup struct {
-	FileName    string
-	FileContent []byte
-	FilePerm    os.FileMode
+type fileDesc struct {
+	Name    string
+	Content string
 }
 
 func Test_Request_ExtendWithContent(t *testing.T) {
 	testCases := []struct {
-		Setup []testFileSystemSetup
-		Error error
 		Group string
+		Files []fileDesc // Files relative to the group directory.
 		Units []controller.Unit
+		Error error
 	}{
 		// This test ensures that loading a single unit from a directory results in
 		// the expected controller request.
 		{
-			Setup: []testFileSystemSetup{
-				{
-					FileName:    "dirname/dirname_unit.service",
-					FileContent: []byte("some unit content"),
-					FilePerm:    os.FileMode(0644),
-				},
+			Group: "dirname",
+			Files: []fileDesc{
+				{Name: "dirname/dirname_unit.service", Content: "some unit content"},
+			},
+			Units: []controller.Unit{
+				{Name: "dirname_unit.service", Content: "unchecked"},
 			},
 			Error: nil,
-			Group: "dirname",
-			Units: []controller.Unit{
-				{
-					Name:    "dirname_unit.service",
-					Content: givenSomeUnitFileContent(),
-				},
-			},
 		},
 
 		// This test ensures that noUnitFilesError is returned for empty gorup.
 		{
-			Setup: []testFileSystemSetup{},
-			Error: noUnitFilesError,
 			Group: "",
+			Files: []fileDesc{},
 			Units: []controller.Unit{},
+			Error: noUnitFilesError,
 		},
 
 		// This test ensures that trying to load unit files when no files are in
 		// the file system throws an error.
 		{
-			Setup: []testFileSystemSetup{},
-			Error: &os.PathError{
-				Op:   "open",
-				Path: "dirname",
-				Err:  os.ErrNotExist,
-			},
 			Group: "dirname",
+			Files: []fileDesc{},
 			Units: []controller.Unit{},
+			Error: &os.PathError{Op: "open", Path: "dirname", Err: os.ErrNotExist},
 		},
 
 		// This test ensures that folders inside a group folder are ignored
 		{
-			Setup: []testFileSystemSetup{
-				{FileName: "groupname/someotherdiretctory/REAMDE.md", FileContent: []byte("DO NOT READ ME"), FilePerm: os.FileMode(0644)},
-				{FileName: "groupname/groupname-1.service", FileContent: []byte(givenSomeUnitFileContent()), FilePerm: os.FileMode(0644)},
-				{FileName: "groupname/groupname-2.service", FileContent: []byte(givenSomeUnitFileContent()), FilePerm: os.FileMode(0644)},
-			},
 			Group: "groupname",
-			Units: []controller.Unit{
-				{
-					Name:    "groupname-1.service",
-					Content: givenSomeUnitFileContent(),
-				},
-				{
-					Name:    "groupname-2.service",
-					Content: givenSomeUnitFileContent(),
-				},
+			Files: []fileDesc{
+				{Name: "groupname/nesteddir/REAMDE.md", Content: "DO NOT READ ME"},
+				{Name: "groupname/groupname-1.service", Content: "unchecked"},
+				{Name: "groupname/groupname-2.service", Content: "unchecked"},
 			},
+			Units: []controller.Unit{
+				{Name: "groupname-1.service", Content: "unchecked"},
+				{Name: "groupname-2.service", Content: "unchecked"},
+			},
+			Error: nil,
 		},
 	}
 
 	for i, testCase := range testCases {
 		newFileSystem := afero.Afero{Fs: afero.NewMemMapFs()}
 
-		for _, setup := range testCase.Setup {
-			err := newFileSystem.WriteFile(setup.FileName, setup.FileContent, setup.FilePerm)
+		for _, f := range testCase.Files {
+			err := newFileSystem.WriteFile(f.Name, []byte(f.Content), os.FileMode(0644))
 			if err != nil {
 				t.Fatal("case", i+1, "expected", nil, "got", err)
 			}
